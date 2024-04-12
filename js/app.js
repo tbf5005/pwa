b1f3b1eda3ffeebd9a3d3294b5866d21cc63805c
tbf <tbf5005@gmail.com>
2024-04-12 04:05:52 +0330

    set banner install on app.js

--- js/app.js
+++ js/app.js
@@ -7,3 +7,31 @@ if('serviceWorker' in navigator) {
             console.log('Service worker registration failed:' , err);
         })
 }
+
+let installPromptEvent;
+
+window.addEventListener('beforeinstallprompt' , (e) => {
+    e.preventDefault();
+    console.log('before install prompt event')
+    installPromptEvent = e;
+});
+
+
+document.addEventListener("DOMContentLoaded", (e) => {
+    e.preventDefault();
+    console.log(installPromptEvent);
+    if(installPromptEvent) {
+        installPromptEvent.prompt();
+
+        installPromptEvent.userChoice
+            .then((choiceResult) => {
+                if(choiceResult.outcome === 'accepted') {
+                    console.log('User Accepted');
+                } else {
+                    console.log('User dismissed');
+                }
+
+                installPromptEvent = null;
+            })
+    }
+})
